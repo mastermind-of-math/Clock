aad290dc901a1f86a49bd1ab743c75678f88a301
mastermind-of-math <101933690+mastermind-of-math@users.noreply.github.com>
2023-02-07 19:36:23 -0500

    Add files via upload

--- clockcode.js
+++ clockcode.js
@@ -1,8 +1,10 @@
 var time = "";
+var pm = false;
 var hour = 00;
 var mins = 00;
 var secs = 00;
 var mils = 000;
+var fonts = ['monospace', 'Arial'];
 
 Number.prototype.addZeros = function(n){
     for (var i = this.toString(); i.length < n; i = "0" + i);
@@ -18,16 +20,18 @@ function getTime(){
 }
 
 function allZeros(){
-    hour = hour.addZeros(2)
-    mins = mins.addZeros(2)
-    secs = secs.addZeros(2)
-    mils = mils.addZeros(3)
+    hour = hour.addZeros(2);
+    mins = mins.addZeros(2);
+    secs = secs.addZeros(2);
+    mils = mils.addZeros(3);
 }
 
 function displayTime(){
     if(document.getElementById("ampm").checked == false){
+        pm = false;
         if(hour > 12){
-            hour = hour - 12
+            pm = true;
+            hour = hour - 12;
         }
     }
     if(document.getElementById("milli").checked){
@@ -36,38 +40,39 @@ function displayTime(){
         time = hour + ":" + mins + ":" + secs;
     }
     if(document.getElementById("ampm").checked == false){
-        if(hour > 12){
-            time = time + " PM"
+        if(pm){
+            time = time + " PM";
         } else {
-            time = time + " AM"
+            time = time + " AM";
         }
     }
-    document.getElementById("main").innerHTML = time
+    document.getElementById("main").innerHTML = time;
 }
 
 function updateTime(){
-    getTime()
-    allZeros()
-    displayTime()
+    getTime();
+    allZeros();
+    displayTime();
 }
 
 function changed(){
-    document.getElementById("main").style.color = document.getElementById("textcolor").value
-    document.getElementById("main").style.fontSize = document.getElementById("fontsize").value + "px"
-    document.getElementById("backcolor").style.backgroundColor = document.getElementById("backgroundcolor").value
+    document.getElementById("main").style.color = document.getElementById("textcolor").value;
+    document.getElementById("main").style.fontSize = document.getElementById("fontsize").value + "px";
+    document.getElementById("backcolor").style.backgroundColor = document.getElementById("backgroundcolor").value;
+    document.getElementById("label-six").innerText = "Font Family: Azeret Mono";
 }
 
 function reset(){
-    document.getElementById("textcolor").value = "#00b8d9"
-    document.getElementById("fontsize").value = "125"
-    document.getElementById("backgroundcolor").value = "#333333"
-    document.getElementById("ampm").checked = true
-    document.getElementById("milli").checked = true
+    document.getElementById("textcolor").value = "#00b8d9";
+    document.getElementById("fontsize").value = "125";
+    document.getElementById("backgroundcolor").value = "#333333";
+    document.getElementById("backcolor").style.fontFamily = "Azeret Mono";
+    document.getElementById("ampm").checked = true;
+    document.getElementById("milli").checked = true;
     changed()
 }
 
 window.setInterval(updateTime, 1)
-var fullscreen = 0;
 var element = document.documentElement;
 
 function openFull(){
@@ -80,22 +85,28 @@ function openFull(){
     }
 }  
 
-function closeFull(){
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
+function toggle(){
+    openFull()
+}
+
+function selectFont() {
+    document.getElementById("items").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
     }
 }
 
-function toggle(){
-    if(fullscreen = 1){
-        openFull();
-        fullscreen = 0;
-    } else {
-        closeFull();
-
-    }
+function font(id){
+    document.getElementById("backcolor").style.fontFamily = id;
+    document.getElementById("label-six").innerText = "Font Family: " + id;
 }
